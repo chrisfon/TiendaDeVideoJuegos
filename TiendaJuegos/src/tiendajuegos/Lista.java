bb6e6c4097f19b70bacb77521600eb213d77e76f
chrisfon <39479488+chrisfon@users.noreply.github.com>
2019-04-18 17:29:46 -0600

    Arreglo login

---
+++ TiendaJuegos/src/tiendajuegos/Lista.java
@@ -0,0 +1,230 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiendajuegos;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author PJ129
+ */
+
+public class Lista {
+    
+    private Usuario usuario;
+    private NodoLista cabeza;
+    private NodoLista ultimo;
+
+    public void insertarJuego(Juego j) {
+        if (cabeza == null) {
+            cabeza = new NodoLista(j);
+            ultimo = cabeza;
+            ultimo.setNext(cabeza);
+            cabeza.setBack(ultimo);
+
+        } else if (j.getIdJuego() < cabeza.getDatoJuego().getIdJuego()) {
+            NodoLista aux = new NodoLista(j);
+            aux.setNext(cabeza);
+            cabeza.setBack(aux);
+            cabeza = aux;
+            ultimo.setNext(cabeza);
+            cabeza.setBack(ultimo);
+
+        } else if (ultimo.getDatoJuego().getIdJuego() <= j.getIdJuego()) {
+            NodoLista aux = new NodoLista(j);
+            aux.setBack(ultimo);
+            ultimo.setNext(aux);
+            ultimo = ultimo.getNext();
+            ultimo.setNext(cabeza);
+            cabeza.setBack(ultimo);
+
+        } else {
+            NodoLista aux = cabeza;
+            while (aux.getNext().getDatoJuego().getIdJuego() < j.getIdJuego()) {
+                aux = aux.getNext();
+            }
+            NodoLista temp = new NodoLista(j);
+            temp.setNext(aux.getNext());
+            temp.setBack(aux);
+            aux.setNext(temp);
+            temp.getNext().setBack(temp);
+        }
+    }
+    
+    
+   public Juego extrae(int id){
+       Juego p=null;
+      if (cabeza!=null) {
+            if (id>=cabeza.getDatoJuego().getIdJuego() && id<=ultimo.getDatoJuego().getIdJuego()) {
+                if (cabeza.getDatoJuego().getIdJuego()==id) {
+                    if (cabeza.getDatoJuego().getCantidadJuego()>0){
+                     cabeza.getDatoJuego().setCantidadJuego(cabeza.getDatoJuego().getCantidadJuego() - 1);
+                    p=cabeza.getDatoJuego();   
+                    }
+                    
+                }else{
+                     NodoLista aux=cabeza;               
+                while(aux.getNext()!=cabeza){                
+               aux=aux.getNext();               
+                } 
+                if (aux.getDatoJuego().getIdJuego()==id) {
+                   if (aux.getDatoJuego().getCantidadJuego()>0){
+                     aux.getDatoJuego().setCantidadJuego(aux.getDatoJuego().getCantidadJuego() - 1);
+                    p=aux.getDatoJuego();   
+                    }else{
+                    
+                       JOptionPane.showMessageDialog(null,"El juego se encuentra agotado!");
+                    
+                    } 
+                    
+                    if (aux.getNext()== ultimo) {
+                        ultimo=aux;
+                        
+                    }
+                 
+                
+                }
+                    
+                }               
+               
+            }
+            
+        }
+     
+    return p;
+   }
+    public void insertarUsuario(Usuario u){
+        if (cabeza == null) {
+            cabeza = new NodoLista(u);
+            ultimo = cabeza;
+            ultimo.setNext(cabeza);
+            cabeza.setBack(ultimo);
+            
+
+        } else if (u.getUsuarioID() < cabeza.getDatoUsuario().getUsuarioID()) {
+            NodoLista aux = new NodoLista(u);
+            aux.setNext(cabeza);
+            cabeza.setBack(aux);
+            cabeza = aux;
+            ultimo.setNext(cabeza);
+            cabeza.setBack(ultimo);
+
+        } else if (ultimo.getDatoUsuario().getUsuarioID()<= u.getUsuarioID()) {
+            NodoLista aux = new NodoLista(u);
+            aux.setBack(ultimo);
+            ultimo.setNext(aux);
+            ultimo = ultimo.getNext();
+            ultimo.setNext(cabeza);
+            cabeza.setBack(ultimo);
+
+        } else {
+            NodoLista aux = cabeza;
+            while (aux.getNext().getDatoUsuario().getUsuarioID() < u.getUsuarioID()) {
+                aux = aux.getNext();
+            }
+            NodoLista temp = new NodoLista(u);
+            temp.setNext(aux.getNext());
+            temp.setBack(aux);
+            aux.setNext(temp);
+            temp.getNext().setBack(temp);
+        }
+    }
+    
+    public Usuario buscarUsuario (String nombreUsuario, String contrasenaUsuario){
+       Usuario usuario = null;
+       NodoLista aux= cabeza;
+       boolean encontrado = false;
+        if (cabeza!=null) {
+             if (aux.getDatoUsuario().getNombreUsuario().equals(nombreUsuario) && aux.getDatoUsuario().getContraseñaUsuario().equals(contrasenaUsuario)){
+                usuario = aux.getDatoUsuario();
+             }
+             aux = aux.getNext();
+            while(aux != cabeza && !encontrado){
+             if (aux.getDatoUsuario().getNombreUsuario().equals(nombreUsuario) && aux.getDatoUsuario().getContraseñaUsuario().equals(contrasenaUsuario)){
+                usuario = aux.getDatoUsuario();
+                encontrado = true;
+            }else{
+                 usuario = null;
+                 //encontrado = false;
+            }  
+             
+             aux = aux.getNext();
+            }
+            
+            
+        }
+        if (encontrado){
+         return usuario;   
+        }else{
+            return null;
+        }
+        
+    } 
+    
+    
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    @Override
+    public String toString() {
+        String s="";
+       NodoLista aux= cabeza;
+        if (cabeza!=null) {
+            s+=aux+",";
+            aux=aux.getNext();
+            while(aux!=cabeza){
+                s+=aux +",";
+                aux= aux.getNext();
+            }
+        }
+        return s;
+    }
+    
+public Juego extraeSinResta(int id){
+       Juego p=null;
+      if (cabeza!=null) {
+            if (id>=cabeza.getDatoJuego().getIdJuego() && id<=ultimo.getDatoJuego().getIdJuego()) {
+                if (cabeza.getDatoJuego().getIdJuego()==id) {
+                    if (cabeza.getDatoJuego().getCantidadJuego()>0){
+                     
+                    p=cabeza.getDatoJuego();   
+                    }else{
+                         JOptionPane.showMessageDialog(null,"El juego se encuentra agotado!");
+                    }
+                    
+                }else{
+                     NodoLista aux=cabeza;               
+                while(aux.getNext()!=cabeza){                
+               aux=aux.getNext();               
+                } 
+                if (aux.getDatoJuego().getIdJuego()==id) {
+                   if (aux.getDatoJuego().getCantidadJuego()>0){
+                     
+                    p=aux.getDatoJuego();   
+                    }else{
+                        JOptionPane.showMessageDialog(null,"El juego se encuentra agotado!");
+                   }
+                    
+                    if (aux.getNext()== ultimo) {
+                        ultimo=aux;
+                        
+                    }
+                 
+                
+                }
+                    
+                }               
+               
+            }
+            
+        }
+     
+    return p;
+   }
+    
+}
---
+++ TiendaJuegos/src/tiendajuegos/Pilas.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiendajuegos;
+
+/**
+ *
+ * @author crazyiot
+ */
+public class Pilas {
+ private NodoPila top;
+ public int idOrden;
+
+
+    public int getIdOrden() {
+        return idOrden;
+    }
+
+    public void setIdOrden(int idOrden) {
+        this.idOrden = idOrden;
+    }
+
+    public NodoPila getTop() {
+        return top;
+    }
+
+    public void setTop(NodoPila top) {
+        this.top = top;
+    }
+ 
+ 
+ 
+ public void push(NodoPila d) {
+        if (top == null) {
+            top = d;
+        } else {
+            d.setAbajo(top);
+            top = d;
+        }
+    }
+
+    public NodoPila pop() {
+        NodoPila aux = null;
+        if (top != null) {
+            aux = top;
+            top = aux.getAbajo();
+        }
+        return aux;
+    }
+   public void cambio(int id) {
+        NodoPila aux = top;
+        while (aux != null) {
+
+            if (aux.getOrden().getIdOrden() == id) {
+                //que quiere cambiar si se ocupa este metodo
+                break;
+            } else {
+                
+                aux = aux.getAbajo();
+            }
+        }
+        if (aux == null) {
+            System.out.println("**Error, ID no existe en el sistema**");
+          
+        } else {
+            System.out.println("Cambio Exitoso! Lista actualizada:");;
+        }
+       
+        
+    }
+public int montoTotal(){
+int montoTotal = 0;
+
+        NodoPila aux = top;
+        while (aux != null) {
+            montoTotal += aux.getOrden().getJuego().getPrecioJuego();
+            aux = aux.getAbajo();
+        }
+        
+     return montoTotal; 
+}   
+ @Override
+    public String toString() {
+        String s = "";
+        NodoPila aux = top;
+        while (aux != null) {
+            s += aux + "\n";
+            aux = aux.getAbajo();
+        }
+        return s;
+    }
+}
